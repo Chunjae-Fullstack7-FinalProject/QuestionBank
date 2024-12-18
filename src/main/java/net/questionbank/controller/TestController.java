9493af4c0d1ac091973a0e488046e2ffc06468bf
SumiSong <mjsong2001@naver.com>
2024-12-18 13:55:52 +0900

    feat: step2 템플릿 추가

--- src/main/java/net/questionbank/controller/TestController.java
+++ src/main/java/net/questionbank/controller/TestController.java
@@ -3,9 +3,22 @@ package net.questionbank.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 @Log4j2
+@RequestMapping("/step2")
 public class TestController {
+
+    /*
+    문항 편집(문제 목록, 문제지요약)
+    step1에서 받은 과목 id 값
+    step2/sub03_01_01로 리턴
+     */
+    @GetMapping("/editQuestion")
+    public String getItemIds(){
+        return "test/step2";
+    }
 }
