746ac1fe67ccea2f6f0205634d7e77c21e9bed1b
JerryHan <haninqq@gmail.com>
2024-12-18 15:15:32 +0900

    docs: Modify Textbook Entity

---
+++ src/main/java/net/questionbank/controller/Test.java
@@ -0,0 +1,18 @@
+package net.questionbank.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@Log4j2
+@RequestMapping("/admin")
+public class Test {
+    @GetMapping("/asdf")
+    public String asdf() {
+        return "common/common";
+    }
+}
